4d0dd70b5f167407647d862e98b5ce4c4dcd1e75
Gheorghe Varga <149835163+0728george@users.noreply.github.com>
2025-11-08 01:22:33 -0500

    Update library.js

--- js/library.js
+++ js/library.js
@@ -1,1 +1,15 @@
-
+async function fetchBooks() {
+  const response = await fetch('https://docs.google.com/spreadsheets/d/SHEET_ID/gviz/tq?tqx=out:json');
+  const text = await response.text();
+  const data = JSON.parse(text.substr(47).slice(0, -2)); // Parse Google Viz JSON
+  return data.table.rows.map(row => ({
+    title: row.c[0].v,
+    genre: row.c[1].v,
+    year: row.c[2].v,
+    thumbnail: row.c[3].v,
+    url: row.c[4].v,
+    format: row.c[5].v,
+    likes: row.c[6].v,
+    ratings: row.c[7].v
+  }));
+}
